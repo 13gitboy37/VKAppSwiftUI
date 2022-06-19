3413891a1c5fc5fd6958209032e0c2c72182ce14
nikitamoshentsev <trane0909@bk.ru>
2022-06-19 12:04:17 +0300

    Initial Commit

---
+++ VKAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VKAppSwiftUI
+//
+//  Created by Никита Мошенцев on 19.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VKAppSwiftUI/VKAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKAppSwiftUIApp.swift
+//  VKAppSwiftUI
+//
+//  Created by Никита Мошенцев on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
